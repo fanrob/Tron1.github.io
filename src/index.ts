3088da14c0e93d3cfc7bb7bd5b3ead7fa8f91cf9
fanrob <yedipro@yandex.ru>
2024-05-22 22:31:25 +0300

    make classes

--- src/index.ts
+++ src/index.ts
@@ -5,52 +5,49 @@ import { Vector3 } from '@babylonjs/core/Maths/math.vector';
 import { CreateGround } from '@babylonjs/core/Meshes/Builders/groundBuilder';
 import { CreateSphere } from '@babylonjs/core/Meshes/Builders/sphereBuilder';
 import { Scene } from '@babylonjs/core/scene';
-
+import {MeshBuilder} from '@babylonjs/core/Meshes/meshBuilder';
 import { GridMaterial } from '@babylonjs/materials/grid/gridMaterial';
 
-// Get the canvas element from the DOM.
-const canvas = document.getElementById("renderCanvas") as HTMLCanvasElement;
 
-// Associate a Babylon Engine to it.
-const engine = new Engine(canvas);
+let canvas:HTMLCanvasElement;
+let engine:Engine;
+let scene:Scene;
 
-// Create our first scene.
-var scene = new Scene(engine);
+class Game {
+ 
+  constructor(){
+    canvas = document.getElementById("renderCanvas") as HTMLCanvasElement;
+    engine = new Engine(canvas);
+    scene = new Scene(engine);
+    let camera = new FreeCamera("camera1", new Vector3(0, 5, -10), scene);
+    camera.setTarget(Vector3.Zero());
+    camera.attachControl(canvas, true);
+    
+    engine.runRenderLoop(this.renderLoop);
+  }
 
-// This creates and positions a free camera (non-mesh)
-var camera = new FreeCamera("camera1", new Vector3(0, 5, -10), scene);
 
-// This targets the camera to scene origin
-camera.setTarget(Vector3.Zero());
+  renderLoop(){
+    scene.render();
+  }
 
-// This attaches the camera to the canvas
-camera.attachControl(canvas, true);
+  makeCube(x:number,y:number,size:number){
+    let mesh = MeshBuilder.CreateBox("box",{size},scene);
+    mesh.material = new GridMaterial("grid", scene);
+    return mesh;
+  }
 
-// This creates a light, aiming 0,1,0 - to the sky (non-mesh)
-var light = new HemisphericLight("light1", new Vector3(0, 1, 0), scene);
+  createLightSimple(){
+    let light = new HemisphericLight("light1", new Vector3(0, 1, 0), scene);
+    light.intensity = 0.7;
+  }
 
-// Default intensity is 1. Let's dim the light a small amount
-light.intensity = 0.7;
+}
 
-// Create a grid material
-var material = new GridMaterial("grid", scene);
+let PGame = new Game();
+PGame.makeCube(0,-100,10);
+PGame.createLightSimple();
 
-// Our built-in 'sphere' shape.
-var sphere = CreateSphere('sphere1', { segments: 16, diameter: 2 }, scene);
 
-// Move the sphere upward 1/2 its height
-sphere.position.y = 2;
 
-// Affect a material
-sphere.material = material;
 
-// Our built-in 'ground' shape.
-var ground = CreateGround('ground1', { width: 6, height: 6, subdivisions: 2 }, scene);
-
-// Affect a material
-ground.material = material;
-
-// Render every frame
-engine.runRenderLoop(() => {
-  scene.render();
-});
